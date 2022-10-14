e11ec850497e455bdf564516b5b2c47c0d8871ff
TimothyLor <lortg@gmatc.matc.edu>
2022-10-14 17:57:29 -0500

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog.`,
+    author: `Timothy Lor`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Timothy Lor`,
+      company: `Blogs Inc.`,
+      address: `PO box 1234`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
